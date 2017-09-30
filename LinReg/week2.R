18ca95dde85837012ba9bdb71a980e2fb0544466
Yulia Zamriy <yulia.zamriy@gmail.com>
2017-09-30 10:31:58 -0400

    updated

--- LinReg/week2.R
+++ LinReg/week2.R
@@ -119,3 +119,74 @@ summary(fit)$sigma
 sqrt(sum(resid(fit)^2) / (n - 2))
 
 example("anscombe")
+
+data(diamond)
+
+y <- diamond$price
+x <- diamond$carat
+n <- length(y)
+
+beta1 <- cor(y, x) * sd(y) / sd(x)
+beta0 <- mean(y) - beta1 * mean(x)
+e <- y - beta0 - beta1 * x
+
+sigma <- sqrt(sum(e^2) / (n-2)) 
+ssx <- sum((x - mean(x))^2)
+
+seBeta0 <- (1 / n + mean(x) ^ 2 / ssx) ^ .5 * sigma 
+seBeta1 <- sigma / sqrt(ssx)
+tBeta0 <- beta0 / seBeta0
+tBeta1 <- beta1 / seBeta1
+pBeta0 <- 2 * pt(abs(tBeta0), df = n - 2, lower.tail = FALSE)
+pBeta1 <- 2 * pt(abs(tBeta1), df = n - 2, lower.tail = FALSE)
+
+coefTable <- rbind(c(beta0, seBeta0, tBeta0, pBeta0), c(beta1, seBeta1, tBeta1, pBeta1))
+colnames(coefTable) <- c("Estimate", "Std. Error", "t value", "P(>|t|)")
+rownames(coefTable) <- c("(Intercept)", "x")
+coefTable
+
+fit <- lm(y ~ x); 
+summary(fit)$coefficients
+
+sumCoef <- summary(fit)$coefficients
+sumCoef[1,1] + c(-1, 1) * qt(.975, df = fit$df) * sumCoef[1, 2]
+(sumCoef[2,1] + c(-1, 1) * qt(.975, df = fit$df) * sumCoef[2, 2]) / 10
+
+newx = data.frame(x = seq(min(x), max(x), length = 100))
+p1 = data.frame(predict(fit, newdata= newx,interval = ("confidence")))
+p2 = data.frame(predict(fit, newdata = newx,interval = ("prediction")))
+p1$interval = "confidence"
+p2$interval = "prediction"
+p1$x = newx$x
+p2$x = newx$x
+dat = rbind(p1, p2)
+names(dat)[1] = "y"
+
+g = ggplot(dat, aes(x = x, y = y))
+g = g + geom_ribbon(aes(ymin = lwr, ymax = upr, fill = interval), alpha = 0.2) 
+g = g + geom_line()
+g = g + geom_point(data = data.frame(x = x, y=y), aes(x = x, y = y), size = 4)
+g
+
+# swirl
+
+fit <- lm(child ~ parent, galton)
+n <- 928
+sqrt(sum(fit$residuals^2)/(n-2))
+# same as
+summary(fit)$sigma
+# same as
+sqrt(deviance(fit)/(n-2))
+# [1] 2.238547
+
+mu <- mean(galton$child)
+sTot <- sum((galton$child - mu)^2)
+sRes <- deviance(fit)
+1 - sRes/sTot
+# same as
+summary(fit)$r.squared
+# [1] 0.2104629
+
+cor(galton$child,galton$parent)^2
+# [1] 0.2104629
+
